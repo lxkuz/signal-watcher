e929eaa85aa49c1e1252c12b313d0fc9056c5388
Alexey Kuznetsov <a.kuznetsov@okft.io>
2024-05-14 23:21:10 +0500

    Add step signal

--- main.rb
+++ main.rb
@@ -18,24 +18,37 @@ def add_spike_1(signal, size, offset)
   signal
 end
 
+# _/\_
+def add_step(signal, size, offset)
+  res = []
+  signal.each_with_index do |v, i|
+    if i >= offset
+      res.push(v + size)
+    else
+      res.push(v)
+    end
+  end
+  res
+end
+
 def start
-  dataset_size = 200_000
-  file = File.open("./data/signal_#{dataset_size}.csv", 'w')
-  len = 100
+  dataset_size = 100_000
+  len = 500
   value = 50
   noize = 5
-  spike_size = 10
+  spike_size = 12
+  file = File.open("./data/signal_#{dataset_size}_#{len}.csv", 'w')
 
-  file.write("#{(0...len).map{|i| "s#{i + 1}" }.join(',')},#{(0...len).map{|i| "spike#{i + 1}" }.join(',')}\n")
+  file.write("#{(0...len).map{|i| "s#{i + 1}" }.join(',')},#{(0...len).map{|i| "spike#{i + 1}" }.join(',')},no_spike\n")
   progressbar = ProgressBar.create(total: dataset_size)
 
   dataset_size.times do
     progressbar.increment
     signal = generate_signal(len, noize, value)
-    file.write("#{signal.join(',')},#{(0...len).map{|_| 0}.join(',')}\n")
+    file.write("#{signal.join(',')},#{(0...len).map{|_| 0}.join(',')},1\n")
     offset = rand(len)
-    signal_with_spike = add_spike_1(signal, spike_size, offset)
-    file.write("#{signal_with_spike.join(',')},#{(0...len).map{|i| i == offset ? 1 : 0 }.join(',')}\n")
+    signal_with_spike = add_step(signal, spike_size, offset)
+    file.write("#{signal_with_spike.join(',')},#{(0...len).map{|i| i == offset ? 1 : 0 }.join(',')},0\n")
   end
   file.close
 end
